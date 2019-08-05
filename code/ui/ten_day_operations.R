8e9d7d2cb74462ef268311443326c326d636b306
ShyGuyPy <luke.vawter1@gmail.com>
2019-08-05 15:22:11 -0400

    still missing some pieces, but those included thus far are all essential components

---
+++ code/ui/ten_day_operations.R
@@ -0,0 +1,9 @@
+tabPanel("Ten Day",
+         fluidRow(
+           align = "center",
+           plotOutput("ten_day", height = plot.height, width = plot.width),
+           br()
+         ) # End fluidRow        
+         
+         
+) # end of tab panel
